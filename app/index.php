c774fcd5775803ed183b87b9c701ee541df65dbc
bsmart <brit@opusoneinteractive.com>
2014-07-21 22:09:50 -0400

    setup typesettings removed windows.js

---
+++ app/index.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <title>irodetoday</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+        <link rel="shortcut icon" href="/favicon.ico">
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+        <!-- build:css styles/vendor.css -->
+        <!-- bower:css -->
+        <!-- endbower -->
+        <!-- endbuild -->
+        <!-- build:css(.tmp) styles/main.css -->
+        <link rel="stylesheet" href="styles/main.css">
+        <!-- endbuild -->
+    </head>
+    <body>
+        <!--[if lt IE 10]>
+        <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <div class="container">
+            <section class="intro window">
+                <div class="flexslider">
+                  <ul class="slides">
+                    <li>
+                      <img src="http://placehold.it/350x150" />
+                    </li>
+                    <li>
+                      <img src="http://placehold.it/350x150" />
+                    </li>
+                    <li>
+                      <img src="http://placehold.it/350x150" />
+                    </li>
+                    <li>
+                      <img src="http://placehold.it/350x150" />
+                    </li>
+                  </ul>
+                </div>
+<!--                 <header>
+                    <div class="intro_callout">
+                        <h1>irodetoday</h1>
+                        <p>Lorem ipsum dolor sit amet, ut noster eripuit mea, mel te odio appetere. Nulla omnes ea pro.</p>
+                    </div>
+                </header> -->
+                <div class="scroll-down">
+                    <a href="" class="scroll-down_btn">find out more</a>
+                    <span></span>
+                </div>
+            </section>
+            <section class="window"><img src="http://placehold.it/350x150"></section>
+            <section class="window"><img src="http://placehold.it/350x150"></section>
+            <section class="window"><img src="http://placehold.it/350x150"></section>
+            <section class="window"></section>
+            <section class="window"></section>
+        </div>
+<!-- build:js scripts/vendor.js -->
+<!-- bower:js -->
+<script src="bower_components/jquery/dist/jquery.js"></script>
+<script src="bower_components/modernizr/modernizr.js"></script>
+<!-- endbower -->
+<!-- endbuild -->
+<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+<script>
+(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+e.src='//www.google-analytics.com/analytics.js';
+r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+ga('create','UA-XXXXX-X');ga('send','pageview');
+</script>
+<!-- build:js({app,.tmp}) scripts/main.js -->
+<script src="scripts/main.js"></script>
+<!--  <script src="scripts/preloadassets.js"></script> -->
+<script src="scripts/windows.js"></script>
+<script src="scripts/flexslider.js"></script>
+<!-- <script src="scripts/XHRLoader.js"></script> -->
+<!-- endbuild -->
+</body>
+</html>
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,26 @@
+// Windows https://github.com/nick-jonas/nick-jonas.github.com/tree/master/windows
 $(document).ready(function() {
-    'use strict';
-    $('.preload').fadeOut(2000, function() {
-        $('.container').fadeIn(1000);
+
+    $('.window').windows({
+        snapping: true,
+        snapSpeed: 500,
+        snapInterval: 1100,
+        onScroll: function(scrollPos) {
+            // scrollPos:Number
+        },
+        onSnapComplete: function($el) {
+            // after window ($el) snaps into place
+        },
+        onWindowEnter: function($el) {
+            // when new window ($el) enters viewport
+        }
+    })
+
+});
+
+
+$(window).load(function() {
+    $('.flexslider').flexslider({
+        animation: "slide"
     });
 });
